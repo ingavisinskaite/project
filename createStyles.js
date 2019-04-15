40626f28228ab26f9d79c402114dd273e7ebcddd
ingavisinskaite <ingaqvi@gmail.com>
2019-04-15 13:05:23 +0300

    Fixed 'create styles' button and  content size

--- createStyles.js
+++ createStyles.js
@@ -22,7 +22,7 @@ function applyMenuStyleRules(sheet) {
     sheet.insertRule('.menu > ul > li {display: inline-flex; flex-direction: row; margin: 0 15px;}');
     sheet.insertRule('div:not(.mobile) #signup {background-color: white; color: rgb(18, 60, 204); padding: 7px 20px; border-radius: 4px;}');
     sheet.insertRule(' .logo {float: left; padding-left: 100px; width: 40px;}');
-    sheet.insertRule('.logo > img {margin-right: 10px;}');
+    sheet.insertRule('.logo > img {margin-right: 10px; cursor: pointer;}');
 
 }
 
@@ -31,7 +31,7 @@ function applyMobileMenuStyleRules(sheet) {
     sheet.insertRule('.mobile > ul > li {display: block; padding: 5px; border: 1px solid rgb(18, 60, 204);}');
     sheet.insertRule('.mobile > ul > li:hover {background-color: rgb(228, 219, 219)}');
     sheet.insertRule('.mobile > ul {position: absolute; background-color: white; padding: 0; border-radius: 5px;}');
-    sheet.insertRule('.menu-button {color: rgb(18, 60, 204); background-color: rgb(228, 219, 219); border-radius: 5px; padding: 5px 20px; position: relative; margin-top: 7px;}');
+    sheet.insertRule('.menu-button {color: rgb(18, 60, 204); background-color: rgb(228, 219, 219); border-radius: 5px; padding: 5px 20px; position: relative; margin-top: 7px; cursor: pointer;}');
     sheet.insertRule('.mobile {display: inline-flex; margin-right: 50px; float: right}');
     sheet.insertRule('.dropdown {display: none;}')
 }
@@ -39,8 +39,8 @@ function applyMobileMenuStyleRules(sheet) {
 function applyContentStyleRules(sheet) {
     sheet.insertRule('.content {width: 30%; padding-left: 100px; padding-top: 20x; font-family: "Noto Sans TC", sans-serif;}');
     sheet.insertRule('.content > h1 {font-weight: 900; letter-spacing: 2px;}');
-    sheet.insertRule('.dot {height: 10px; width: 10px; margin-top: 40px; margin-left: 7px; margin-right: 7px;}');
-    sheet.insertRule('.dot:hover{ border: 3px solid rgb(253, 186, 0); margin-top: 57px; margin-left: 4px; margin-top: 37px;}');
-    sheet.insertRule('.btn {margin-top: 50px; color: white; background-color:rgb(217, 36, 48); border: none; border-radius: 3px; padding: 10px;}')
+    sheet.insertRule('.dot {height: 15px; width: 15px; background-color: #4162d6; border-radius: 50%; display: inline-flex; margin-top: 60px; margin-left: 10px; margin-right: 10px;}');
+    sheet.insertRule('.dot:hover{border: 3px solid rgb(253, 186, 0); margin-top: 57px; margin-left: 4px; margin-top: 37px;}');
+    sheet.insertRule('.btn {margin-top: 50px; color: white; background-color: #ff5969; border: none; border-radius: 3px; padding: 10px; cursor: pointer;}')
 }
 
