a32ef79aed968c5e1606a652bdac9c8754d716a7
ingavisinskaite <ingaqvi@gmail.com>
2019-04-28 16:31:55 +0300

    Homepage slider, mobile menu fixes, separate JSONs

--- load.js
+++ load.js
@@ -1,8 +1,16 @@
 "use strict";
 
-//ADDING JSON
+let dotContents = [];
+let menuContents = [];
+let createDesignPageContent = {};
+
+let dotsSliderInterval = {};
+let currentDotContentIndex = 0;
+let homePageActive = true;
+
 function getElement(elObject){
     let element = document.createElement(elObject.type);
+    
     if(elObject.id){
         element.id = elObject.id;
     }
@@ -18,8 +26,20 @@ function getElement(elObject){
     if(elObject.innerHTML){
         element.innerHTML = elObject.innerHTML;
     }
-    if(elObject.onclick){
-        element.onclick = elObject.onclick;
+    if(elObject.action){
+        element.action = elObject.action;
+    }
+    if(elObject.method){
+        element.method = elObject.method;
+    }
+    if(elObject.inputType){
+        element.type = elObject.inputType;
+    }
+    if(elObject.placeholder){
+        element.placeholder = elObject.placeholder;
+    }
+    if(elObject.value){
+        element.value = elObject.value;
     }
     return element;
 }
@@ -37,10 +57,10 @@ function recurseJSON(currentElement, parent){
     }
 }
 
-function loadJSON(callback) {
+function loadJSON(callback, url) {
     let req = new XMLHttpRequest();
     req.overrideMimeType('application/json');
-    req.open('GET', 'https://api.myjson.com/bins/fwua0', true); 
+    req.open('GET', url, true); 
     req.onreadystatechange = 
         function() {
             if (req.readyState == 4 && req.status == "200") {
@@ -52,14 +72,65 @@ function loadJSON(callback) {
 
 function loadPage() {
     loadJSON(function(res) {
-        recurseJSON(JSON.parse(res), document.body);
-        addMobileMenuEventHandlers();
-        addDotsEventHandlers();
-        addMenuEventHandlers();
-    });
+        menuContents = JSON.parse(res);
+        loadJSON(function(res) {
+            recurseJSON(JSON.parse(res), document.body);
+            addMobileMenuEventHandlers();
+            addDotsEventHandlers();
+            addMenuEventHandlers(menuContents);
+            addButtonEventHandler();
+            addLogoEventHandler();
+        }, 'https://api.myjson.com/bins/jcjfw');
+    }, 'https://api.myjson.com/bins/udamc')
+
+    loadJSON(function(res) {
+        createDesignPageContent = JSON.parse(res); 
+    }, 'https://api.myjson.com/bins/1ckpj0');
+
+    loadJSON(function(res) {
+        dotContents = JSON.parse(res);
+        startDotsSlider();
+    }, 'https://api.myjson.com/bins/guip8')
 }
 
-//Mobile Menu
+function startDotsSlider() {
+    dotsSliderInterval = setInterval(function(){
+        changeHomePageContent(currentDotContentIndex);
+        let dots = document.getElementsByClassName('dot');
+        markDotAsSelected(dots[currentDotContentIndex], dots);
+        currentDotContentIndex = getNextDotContentIndex(currentDotContentIndex);
+    }, 3 * 1000);
+}
+
+function stopDotsSlider() {
+    clearInterval(dotsSliderInterval);
+    currentDotContentIndex = 0;
+}
+
+function getNextDotContentIndex(currIndex) {
+    if(currIndex < dotContents.length - 1){
+        let newIndex = currIndex + 1;
+        return newIndex;
+    } else {
+        return 0;
+    }
+}
+
+function addLogoEventHandler() {
+    let logoNode = document.getElementsByClassName('logo')[0];
+    logoNode.addEventListener('click', function() {
+        let menuContentNode = document.getElementsByClassName('content')[0];
+        menuContentNode.innerHTML = '';
+        recurseJSON(menuContents[0], menuContentNode);
+        let pages = document.getElementsByClassName('menu-link');
+        markPageAsSelected(pages[0], pages);
+        addButtonEventHandler();
+        addDotsEventHandlers();
+        stopDotsSlider();
+        startDotsSlider();
+    })
+}
+
 function addMobileMenuEventHandlers() {
     document.getElementsByClassName('content')[0].addEventListener('click', function(){
         let menuDropdown = document.getElementsByClassName('dropdown')[0];
@@ -72,37 +143,84 @@ function addMobileMenuEventHandlers() {
     })
 }
 
-
-//Slider 
 function addDotsEventHandlers() {
     let dots = document.getElementsByClassName('dot');
     for(let i = 0; i < 4; i++){
         dots[i].addEventListener('click', function() {
             markDotAsSelected(this, dots);
+            changeHomePageContent(i);
+            currentDotContentIndex = getNextDotContentIndex(i);
         })
+    }
+}
+
+function changeHomePageContent(contentIndex) {
+    let contentNode = document.getElementsByClassName('text')[0];
+    contentNode.innerHTML = '';
+    let contentToAdd = dotContents[contentIndex];
+    for (let element of contentToAdd) {
+        recurseJSON(element, contentNode);
+    }
+    //button exists in first page
+    if (contentIndex === 0) {
+        addButtonEventHandler();
     }
 }
 
 function markDotAsSelected(selectedDot, allDots) {
     for (let dot of allDots) {
-        dot.classList.remove('selected-dot');
+        dot.removeAttribute('id');
     }
-    selectedDot.classList.add('selected-dot');
+    selectedDot.setAttribute('id', 'selected-dot');
 }
 
-//Menu pages
-function addMenuEventHandlers(){
+function addMenuEventHandlers(menuItems){
     let pages = document.getElementsByClassName('menu-link');
-    for(let i = 0; i < 5; i++){
+
+    for(let i = 0; i < menuItems.length * 2; i++){
         pages[i].addEventListener('click', function(){
-            markPageAsSelected(this, pages);
+            if (!this.classList.contains("mobile-menu")) {
+                markPageAsSelected(this, pages);
+            }
+            //mod needed due to mobile and desktop menu
+            changeMenuPage(i % menuItems.length);
         })
     }
+
 }
 
 function markPageAsSelected(selectedPage, allPages) {
     for(let page of allPages){
-        page.classList.remove('selected');
+        page.removeAttribute('id');
     }
-    selectedPage.classList.add('selected');
+    selectedPage.setAttribute('id', 'selected');
+}
+
+function changeMenuPage(contentIndex) {
+    let contentNode = document.getElementsByClassName('content')[0];
+    contentNode.innerHTML = '';
+    let contentToAdd = menuContents[contentIndex];
+    recurseJSON(contentToAdd, contentNode);
+    if (contentToAdd.id === 'home-page') {
+        addDotsEventHandlers();
+        addButtonEventHandler();
+        startDotsSlider();
+    } else {
+        stopDotsSlider();
+    }
+}
+
+
+function addButtonEventHandler() {
+    let button = document.getElementsByClassName('btn')[0];
+    button.addEventListener('click', function(){
+        createDesign();
+    })
+}
+
+function createDesign() {
+    let contentNode = document.getElementsByClassName('content')[0];
+    contentNode.innerHTML = '';
+    recurseJSON(createDesignPageContent, contentNode);
+    stopDotsSlider();
 }
